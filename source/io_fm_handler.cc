19c70fe8d8f36d3a2e06d89958bf79c888e29776
Colin Morningstar <colin_morningstar@cmu.edu>
2025-01-26 17:34:46 -0500

    Further minor changes in io_fm_handler.cc to deal with sz arrays.

--- source/io_fm_handler.cc
+++ source/io_fm_handler.cc
@@ -1019,12 +1019,12 @@ void IOFMHandler::write(const LattField& output)
 {
  int sizeT=output.bytesPerSite();
  lattinfo.resetBytes(sizeT);
- vector<unsigned int> sz(LayoutInfo::Ndim+2);
+ unsigned int sz[IOFMHandler::Nd+2];
  sz[0]=LayoutInfo::Ndim;
  for (int k=0;k<LayoutInfo::Ndim;k++)
     sz[k+1]=LayoutInfo::getLattExtents()[k];
  sz[LayoutInfo::Ndim+1]=sizeT;
- multi_write(sz.data(),LayoutInfo::Ndim+2);
+ multi_write(sz,LayoutInfo::Ndim+2);
  write_lattice((const char *)(output.getDataConstPtr()),output.bytesPerWord(), lattinfo);
 }
 
@@ -1038,13 +1038,13 @@ void IOFMHandler::write(const std::vector<LattField>& output)
 
 void IOFMHandler::read(LattField& input)
 {
- unsigned int sz[LayoutInfo::Ndim+2];
+ unsigned int sz[IOFMHandler::Nd+2];
  multi_read(sz,LayoutInfo::Ndim+2);
  bool errflag=(sz[0]!=(unsigned int)(LayoutInfo::Ndim));
  for (int k=0;k<LayoutInfo::Ndim;k++)
     errflag=errflag || (sz[k+1]!=(unsigned int)(LayoutInfo::getLattExtents()[k]));
  check_for_failure(errflag, "Invalid lattice read");
- int sizeT=sz[LayoutInfo::Ndim+1];
+ int sizeT=sz[IOFMHandler::Nd+1];
  lattinfo.resetBytes(sizeT);
  input.reset_by_bytes_per_site(sizeT);  // read using precision in file
  int cmplxbytes=input.bytesPerSite()/input.elemsPerSite();
